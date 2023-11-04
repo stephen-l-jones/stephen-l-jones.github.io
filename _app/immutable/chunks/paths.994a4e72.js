8f5386e391c789f0eb9eae6c96bb5235be0c8f98
Stephen Jones <89660176+stephen-l-jones@users.noreply.github.com>
2023-11-04 13:37:35 -0700

    Update website

--- _app/immutable/chunks/paths.994a4e72.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1tt544c)==null?void 0:s.base)??"/business-strategy";var t;const a=((t=globalThis.__sveltekit_1tt544c)==null?void 0:t.assets)??e;export{a,e as b};
