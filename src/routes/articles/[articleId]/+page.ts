88ef9023f062d030146476e7df940a63746efd1b
Stephen Jones <89660176+stephen-l-jones@users.noreply.github.com>
2023-11-09 09:42:04 -0800

    update img

--- src/routes/articles/[articleId]/+page.ts
+++ src/routes/articles/[articleId]/+page.ts
@@ -1,17 +1,11 @@
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 import { base } from '$app/paths';
+import { emptyNavPage } from '$lib/constants';
 
 export const load: PageLoad = async ({ params, parent, fetch }) => {
     let navPages = (await parent()).navPages;
-    let navPage = navPages.find(navPage => navPage.slug === params.articleId) || {
-        slug: '' as string,
-        folder: '' as string,
-        strap: '' as string,
-        title: '' as string,
-        subtitle: '' as string,
-        iref: '' as string
-    } as NavPage;
+    let navPage = navPages.find(navPage => navPage.slug === params.articleId) || emptyNavPage;
 
     let md = await fetch(`${base}/markdown/${params.articleId}.md`).catch(() => {
         throw error(404, 'Not found');
