849e1e21ee5ef78fe62972decb9010597e2f0bd5
stephen-l-jones <stephen.l.jones@gmail.com>
2023-11-10 20:06:21 -0800

    clean repo in preperation for transfer

--- _app/immutable/chunks/constants.406077f4.js
+++
@@ -1,1 +0,0 @@
-const t=["Customer","Product","Finance","Operations","Partner","Competitor","Environment"],e={slug:"",folder:"",strap:"",title:"",subtitle:"",iref:""},a={min:320,max:640};export{e,a as m,t};
